f59fef37ec3d3b5bc3f73addcea5f166589d74e2
Your Name <you@example.com>
2018-01-12 12:21:48 -0500

    Added Bootstrap files and needed images to the assets folder. Set up routing. Created a components and serices folder. Created components for the nav-bar, footer, and needed pages. Routing is working correctly.

--- myMediaStorage/myMediaStorage/src/app/app.module.ts
+++ myMediaStorage/myMediaStorage/src/app/app.module.ts
@@ -5,17 +5,43 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'upload', component: UploadComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'contactUs', component: ContactUsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UploadComponent,
+    ProfileComponent,
+    ContactUsComponent,
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
